c121e88688401323187c8454005422474938b173
Artem Kislov <akislov.im@gmail.com>
2022-09-30 00:20:58 +0400

    added experimental context project id variable

--- packages/server/src/resolvers/queries/project.ts
+++ packages/server/src/resolvers/queries/project.ts
@@ -2,8 +2,9 @@ import {QueryResolvers} from "../../resolvers-types";
 import {prisma} from "../../prisma";
 import {UserInputError} from "apollo-server";
 
-export const project: QueryResolvers['project'] = async (parent, args) => {
+export const project: QueryResolvers['project'] = async (parent, args, ctx: any) => {
     const project = await prisma.project.findUnique({ where: { id: args.id } })
     if (!project) throw new UserInputError(`No profile id = ${args.id}`)
+    ctx.projectId = project.id
     return project;
 }
--- packages/server/src/resolvers/types/Developer.ts
+++ packages/server/src/resolvers/types/Developer.ts
@@ -2,7 +2,11 @@ import {DeveloperResolvers} from "../../resolvers-types";
 import {prisma} from "../../prisma";
 
 export const Developer: DeveloperResolvers = {
-    projects: async (parent, args) => {
-        return await prisma.project.findMany({ where: { developerId: parent.id } })
+    projects: async (parent, args, ctx: any) => {
+        return await prisma.project.findMany({
+            where: {
+                id: args.siblings ? { not: ctx.projectId } : {},
+                developerId: parent.id,
+            } })
     }
 }
